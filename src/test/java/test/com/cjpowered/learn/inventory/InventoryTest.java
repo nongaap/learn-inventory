dae6b05d3022cab21fc6fe0f3c1c118a7192975d
Greg Wiley <aztec.rex@jammm.com>
2017-04-30 18:38:26 -0700

    remove unused fake db

--- src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
+++ src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
@@ -3,14 +3,11 @@ package test.com.cjpowered.learn.inventory;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
 
-import com.cjpowered.learn.inventory.InventoryDatabase;
 import com.cjpowered.learn.inventory.InventoryManager;
-import com.cjpowered.learn.inventory.Item;
 import com.cjpowered.learn.inventory.Order;
 import com.cjpowered.learn.inventory.ace.AceInventoryManager;
 
@@ -25,17 +22,10 @@ public class InventoryTest {
     @Test
     public void whenNoStockItemsDoNotOrder() {
         // given
-        @SuppressWarnings("unused")
-        final InventoryDatabase fakeDb = new DatabaseTemplate() {
-            @Override
-            public List<Item> stockItems() {
-                return Collections.emptyList();
-            }
-        };
+        final LocalDate today = LocalDate.now();
         final InventoryManager im = new AceInventoryManager();
 
         // when
-        final LocalDate today = LocalDate.now();
         final List<Order> actual = im.getOrders(today);
 
         // then
