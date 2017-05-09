f319d6bc7f93e4ee57477fb73257383ddba68b56
Mike Puangmalai <mike@nongaap.com>
2017-05-09 11:16:19 -0700

    Test for order enough stock, too much stock, and enough stock created.

--- src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
+++ src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
@@ -3,12 +3,16 @@ package test.com.cjpowered.learn.inventory;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
 
+import com.cjpowered.learn.inventory.InventoryDatabase;
 import com.cjpowered.learn.inventory.InventoryManager;
+import com.cjpowered.learn.inventory.Item;
 import com.cjpowered.learn.inventory.Order;
+import com.cjpowered.learn.inventory.StockedItem;
 import com.cjpowered.learn.inventory.ace.AceInventoryManager;
 
 /*
@@ -22,14 +26,105 @@ public class InventoryTest {
     @Test
     public void whenNoStockItemsDoNotOrder() {
         // given
-        final LocalDate today = LocalDate.now();
-        final InventoryManager im = new AceInventoryManager();
+    	final InventoryDatabase db = new DatabaseTemplate(){
+    		@Override
+    		public List<Item> stockItems() {
+    			return Collections.emptyList();
+    		}
+    	};
+    	final LocalDate today = LocalDate.now();
+        final InventoryManager im = new AceInventoryManager(db);
 
         // when
         final List<Order> actual = im.getOrders(today);
 
         // then
         assertTrue(actual.isEmpty());
+
+    }
+    
+    @Test
+    public void orderEnoughStock(){
+    	// given
+    	final int onHand = 10;
+    	final int shouldHave = 16;
+    	Item item = new StockedItem(shouldHave);
+    	final InventoryDatabase db = new DatabaseTemplate(){
+    		@Override
+    		public int onHand(Item item) {
+    			// TODO Auto-generated method stub
+    			return onHand;
+    		}
+    		@Override
+    		public List<Item> stockItems() {
+    			return Collections.singletonList(item);
+    		}
+    	};
+    	final InventoryManager im = new AceInventoryManager(db);
+    	final LocalDate today = LocalDate.now();
+    	
+    	// when
+    	final List<Order> actual = im.getOrders(today);
+    	
+    	// then
+    	assertEquals(1, actual.size());
+    	assertEquals(item, actual.get(0).item);
+    	assertEquals(shouldHave - onHand, actual.get(0).quantity);
+    	
+    }
+    
+    @Test
+    public void tooMuchStock(){
+    	// given
+    	final int onHand = 20;
+    	final int shouldHave = 16;
+    	Item item = new StockedItem(shouldHave);
+    	final InventoryDatabase db = new DatabaseTemplate(){
+    		@Override
+    		public int onHand(Item item) {
+    			// TODO Auto-generated method stub
+    			return onHand;
+    		}
+    		@Override
+    		public List<Item> stockItems() {
+    			return Collections.singletonList(item);
+    		}
+    	};
+    	final InventoryManager im = new AceInventoryManager(db);
+    	final LocalDate today = LocalDate.now();
+    	
+    	// when
+    	final List<Order> actual = im.getOrders(today);
+    	
+    	// then
+    	assertEquals(0, actual.size());
+    }
+    
+    @Test
+    public void enoughStock(){
+    	// given
+    	final int onHand = 16;
+    	final int shouldHave = 16;
+    	Item item = new StockedItem(shouldHave);
+    	final InventoryDatabase db = new DatabaseTemplate(){
+    		@Override
+    		public int onHand(Item item) {
+    			// TODO Auto-generated method stub
+    			return onHand;
+    		}
+    		@Override
+    		public List<Item> stockItems() {
+    			return Collections.singletonList(item);
+    		}
+    	};
+    	final InventoryManager im = new AceInventoryManager(db);
+    	final LocalDate today = LocalDate.now();
+    	
+    	// when
+    	final List<Order> actual = im.getOrders(today);
+    	
+    	// then
+    	assertEquals(0, actual.size());
 
     }
 
