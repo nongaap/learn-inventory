6e98f169dd9626495a9e3e354de54235632a15cd
Mike Puangmalai <mike@nongaap.com>
2017-05-18 16:08:21 -0700

    order enough stock with seasonal stock thats on sale and units already on order test

--- src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
+++ src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
@@ -665,5 +665,42 @@ public class InventoryTest {
     	Set<Order> expected = Collections.singleton(expectedOrder);
     	assertEquals(expected, new HashSet<>(actual));
     }
+    
+    @Test
+    public void orderEnoughStockWithSeasonalStockOnSaleAndAlreadyOnOrder(){
+    	// given
+    	final int onHand = 10;
+    	final int shouldHave = 16;
+    	final int unitsPerPackage = 1;
+    	final Season season = Season.Summer;
+    	final int onOrder = 5;
+    	final boolean orderFirstDayOfMonthOnly = false;
+    	Item item = new SeasonalItem(shouldHave, season, unitsPerPackage, orderFirstDayOfMonthOnly);
+    	final HashMap<Item, Integer> store = new HashMap<>();
+    	store.put(item,  onHand);
+    	final HashMap<Item, Integer> onOrderStore = new HashMap<>();
+    	onOrderStore.put(item,  onOrder);
+    	final InventoryDatabase db = new FakeDatabase(store, onOrderStore);
+    	final MarketingInfo marketingInfo = new MarketingTemplate(){
+    		@Override
+    		public boolean onSale(Item item) {
+    			return true;
+    		}
+    		@Override
+    		public Season season(LocalDate when) {
+    			return season;
+    		}
+    	};
+    	final InventoryManager im = new AceInventoryManager(db, marketingInfo);
+    	final LocalDate today = LocalDate.now();
+    	
+    	// when
+    	final List<Order> actual = im.getOrders(today);
+    	
+    	// then
+    	Order expectedOrder = new Order(item, shouldHave + 20 - onHand - onOrder);
+    	Set<Order> expected = Collections.singleton(expectedOrder);
+    	assertEquals(expected, new HashSet<>(actual));
+    }
 
 }
